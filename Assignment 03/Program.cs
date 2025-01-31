220eb9897e77de4c2df05c3f90e189f00100cd80
moomen mohammmed <monm73530@gmail.com>
2025-01-31 20:08:58 +0200

    7-Create an employee directory where employee IDs (int) are keys and employee names (string) are values. Use a SortedList to manage and retrieve employees in order of their IDs.

--- Assignment 03/Program.cs
+++ Assignment 03/Program.cs
@@ -25,7 +25,6 @@
 
         #endregion
 
-
         #region 2-
         //public static string MaxValue(Dictionary<string, int> hashTable)
         //{
@@ -73,26 +72,79 @@
 
         #region 5-
 
-       public static bool ContainsDuplicates(int[] nums)
-        {
-            
-            HashSet<int> seen = new HashSet<int>();
+        //public static bool ContainsDuplicates(int[] nums)
+        // {
 
-            
-            foreach (var num in nums)
-            {
-                
-                if (!seen.Add(num))
-                {
-                    return true;  
-                }
-            }
+        //     HashSet<int> seen = new HashSet<int>();
 
-            return false;
-        }
+
+        //     foreach (var num in nums)
+        //     {
+
+        //         if (!seen.Add(num))
+        //         {
+        //             return true;  
+        //         }
+        //     }
+
+        //     return false;
+        // }
 
         #endregion
-        static void Main(string[] args)
+
+        #region 7-
+
+        static void AddEmployee(SortedList<int, string> directory, int employeeId, string name)
+        {
+            if (!directory.ContainsKey(employeeId))
+            {
+                directory.Add(employeeId, name);
+                Console.WriteLine($"Added employee: ID = {employeeId}, Name = {name}");
+            }
+            else
+            {
+                Console.WriteLine($"Employee with ID {employeeId} already exists.");
+            }
+        }
+
+        static void RemoveEmployee(SortedList<int, string> directory, int employeeId)
+        {
+            if (directory.ContainsKey(employeeId))
+            {
+                directory.Remove(employeeId);
+                Console.WriteLine($"Removed employee with ID = {employeeId}");
+            }
+            else
+            {
+                Console.WriteLine($"Employee with ID {employeeId} not found.");
+            }
+        }
+
+        static string GetEmployeeName(SortedList<int, string> directory, int employeeId)
+        {
+            if (directory.ContainsKey(employeeId))
+            {
+                return directory[employeeId];
+            }
+            else
+            {
+                return $"Employee with ID {employeeId} not found.";
+            }
+        }
+
+        static void ListAllEmployees(SortedList<int, string> directory)
+        {
+            Console.WriteLine("Listing all employees:");
+            foreach (var employee in directory)
+            {
+                Console.WriteLine($"ID = {employee.Key}, Name = {employee.Value}");
+            }
+        }
+
+                #endregion
+
+
+                static void Main(string[] args)
         {
             #region 1-
             //{
@@ -132,14 +184,31 @@
 
             #endregion
 
-
             #region 5-
 
-            int[] nums = { 1, 2, 3, 4, 5, 6};
+            //int[] nums = { 1, 2, 3, 4, 5, 6};
 
-            bool hasDuplicates = ContainsDuplicates(nums);
+            //bool hasDuplicates = ContainsDuplicates(nums);
 
-            Console.WriteLine(hasDuplicates ? "Array contains duplicates." : "Array does not contain duplicates.");
+            //Console.WriteLine(hasDuplicates ? "Array contains duplicates." : "Array does not contain duplicates.");
+
+            #endregion
+
+            #region 7-
+
+            SortedList<int, string> employeeDirectory = new SortedList<int, string>();
+
+            AddEmployee(employeeDirectory, 1001, "Alice");
+            AddEmployee(employeeDirectory, 1003, "Bob");
+            AddEmployee(employeeDirectory, 1002, "Charlie");
+
+            Console.WriteLine("Employee with ID 1002: " + GetEmployeeName(employeeDirectory, 1002));
+
+            RemoveEmployee(employeeDirectory, 1003);
+
+            Console.WriteLine("Employee with ID 1003: " + GetEmployeeName(employeeDirectory, 1003));
+
+            ListAllEmployees(employeeDirectory);
 
             #endregion
         }
