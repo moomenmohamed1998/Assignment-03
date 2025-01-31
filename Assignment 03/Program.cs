30a9a44cf6986893ac5dc392752dfaa96c454356
moomen mohammmed <monm73530@gmail.com>
2025-01-31 20:13:34 +0200

    8-Given an array of integers from 1 to N with some numbers missing, find the missing numbers.

--- Assignment 03/Program.cs
+++ Assignment 03/Program.cs
@@ -94,57 +94,82 @@
 
         #region 7-
 
-        static void AddEmployee(SortedList<int, string> directory, int employeeId, string name)
+        //static void AddEmployee(SortedList<int, string> directory, int employeeId, string name)
+        //{
+        //    if (!directory.ContainsKey(employeeId))
+        //    {
+        //        directory.Add(employeeId, name);
+        //        Console.WriteLine($"Added employee: ID = {employeeId}, Name = {name}");
+        //    }
+        //    else
+        //    {
+        //        Console.WriteLine($"Employee with ID {employeeId} already exists.");
+        //    }
+        //}
+
+        //static void RemoveEmployee(SortedList<int, string> directory, int employeeId)
+        //{
+        //    if (directory.ContainsKey(employeeId))
+        //    {
+        //        directory.Remove(employeeId);
+        //        Console.WriteLine($"Removed employee with ID = {employeeId}");
+        //    }
+        //    else
+        //    {
+        //        Console.WriteLine($"Employee with ID {employeeId} not found.");
+        //    }
+        //}
+
+        //static string GetEmployeeName(SortedList<int, string> directory, int employeeId)
+        //{
+        //    if (directory.ContainsKey(employeeId))
+        //    {
+        //        return directory[employeeId];
+        //    }
+        //    else
+        //    {
+        //        return $"Employee with ID {employeeId} not found.";
+        //    }
+        //}
+
+        //static void ListAllEmployees(SortedList<int, string> directory)
+        //{
+        //    Console.WriteLine("Listing all employees:");
+        //    foreach (var employee in directory)
+        //    {
+        //        Console.WriteLine($"ID = {employee.Key}, Name = {employee.Value}");
+        //    }
+        //}
+
+        #endregion
+
+        #region 8-
+
+        public static List<int> FindMissingNumbers(int[] nums, int N)
         {
-            if (!directory.ContainsKey(employeeId))
+            HashSet<int> existingNumbers = new HashSet<int>();
+
+            foreach (int num in nums)
             {
-                directory.Add(employeeId, name);
-                Console.WriteLine($"Added employee: ID = {employeeId}, Name = {name}");
+                existingNumbers.Add(num);
             }
-            else
+
+            List<int> missingNumbers = new List<int>();
+
+            for (int i = 1; i <= N; i++)
             {
-                Console.WriteLine($"Employee with ID {employeeId} already exists.");
+                if (!existingNumbers.Contains(i))
+                {
+                    missingNumbers.Add(i);
+                }
             }
+
+            return missingNumbers;
         }
 
-        static void RemoveEmployee(SortedList<int, string> directory, int employeeId)
-        {
-            if (directory.ContainsKey(employeeId))
-            {
-                directory.Remove(employeeId);
-                Console.WriteLine($"Removed employee with ID = {employeeId}");
-            }
-            else
-            {
-                Console.WriteLine($"Employee with ID {employeeId} not found.");
-            }
-        }
+        #endregion
 
-        static string GetEmployeeName(SortedList<int, string> directory, int employeeId)
-        {
-            if (directory.ContainsKey(employeeId))
-            {
-                return directory[employeeId];
-            }
-            else
-            {
-                return $"Employee with ID {employeeId} not found.";
-            }
-        }
-
-        static void ListAllEmployees(SortedList<int, string> directory)
-        {
-            Console.WriteLine("Listing all employees:");
-            foreach (var employee in directory)
-            {
-                Console.WriteLine($"ID = {employee.Key}, Name = {employee.Value}");
-            }
-        }
-
-                #endregion
-
-
-                static void Main(string[] args)
+        static void Main(string[] args)
         {
             #region 1-
             //{
@@ -196,19 +221,34 @@
 
             #region 7-
 
-            SortedList<int, string> employeeDirectory = new SortedList<int, string>();
+            //SortedList<int, string> employeeDirectory = new SortedList<int, string>();
 
-            AddEmployee(employeeDirectory, 1001, "Alice");
-            AddEmployee(employeeDirectory, 1003, "Bob");
-            AddEmployee(employeeDirectory, 1002, "Charlie");
+            //AddEmployee(employeeDirectory, 1001, "Alice");
+            //AddEmployee(employeeDirectory, 1003, "Bob");
+            //AddEmployee(employeeDirectory, 1002, "Charlie");
 
-            Console.WriteLine("Employee with ID 1002: " + GetEmployeeName(employeeDirectory, 1002));
+            //Console.WriteLine("Employee with ID 1002: " + GetEmployeeName(employeeDirectory, 1002));
 
-            RemoveEmployee(employeeDirectory, 1003);
+            //RemoveEmployee(employeeDirectory, 1003);
 
-            Console.WriteLine("Employee with ID 1003: " + GetEmployeeName(employeeDirectory, 1003));
+            //Console.WriteLine("Employee with ID 1003: " + GetEmployeeName(employeeDirectory, 1003));
 
-            ListAllEmployees(employeeDirectory);
+            //ListAllEmployees(employeeDirectory);
+
+            #endregion
+
+            #region 8-
+
+            int[] nums = { 1, 2, 4, 6, 7, 8, 10 };
+            int N = 10;  
+
+            List<int> missingNumbers = FindMissingNumbers(nums, N);
+
+            Console.WriteLine("The missing numbers are:");
+            foreach (var num in missingNumbers)
+            {
+                Console.WriteLine(num);
+            }
 
             #endregion
         }
