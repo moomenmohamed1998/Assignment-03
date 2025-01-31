3347b7164f3a962e7c8aa883cc09e8f221b40ca0
moomen mohammmed <monm73530@gmail.com>
2025-01-31 19:22:12 +0200

    4-Given an array of strings, group anagrams together.

--- Assignment 03/Program.cs
+++ Assignment 03/Program.cs
@@ -25,26 +25,51 @@
 
         #endregion
 
+
         #region 2-
-        public static string MaxValue(Dictionary<string, int> hashTable)
-        {
-            string MaxValue = null;
-            int maxValue = int.MinValue;
+        //public static string MaxValue(Dictionary<string, int> hashTable)
+        //{
+        //    string MaxValue = null;
+        //    int maxValue = int.MinValue;
 
-            
-            foreach (var entry in hashTable)
-            {
-                if (entry.Value > maxValue)
-                {
-                    maxValue = entry.Value;
-                    MaxValue = entry.Key;
-                }
-            }
 
-            return MaxValue;
-        }
+        //    foreach (var entry in hashTable)
+        //    {
+        //        if (entry.Value > maxValue)
+        //        {
+        //            maxValue = entry.Value;
+        //            MaxValue = entry.Key;
+        //        }
+        //    }
+
+        //    return MaxValue;
+        //}
         #endregion
 
+        #region 4-
+
+        //public static List<List<string>> Group(string[] strs)
+        //{
+        //   var anagramMap = new Dictionary<string, List<string>>();
+
+        //    foreach (var str in strs)
+        //    {
+        //        char[] chars = str.ToCharArray();
+        //        Array.Sort(chars);
+        //        string sortedStr = new string(chars);
+
+        //        if (!anagramMap.ContainsKey(sortedStr))
+        //        {
+        //            anagramMap[sortedStr] = new List<string>();
+        //        }
+
+        //        anagramMap[sortedStr].Add(str);
+        //    }
+
+        //    return new List<List<string>>(anagramMap.Values);
+        //}
+
+        #endregion
         static void Main(string[] args)
         {
             #region 1-
@@ -57,20 +82,34 @@
 
             #region 2-
 
-            
-            Dictionary<string, int> hashTable = new Dictionary<string, int>
-        {
-            { "a", 1 },
-            { "b", 2 },
-            { "c", 3 },
-            { "d", 2 }
-        };
 
-            
-            string keyWithMaxValue = MaxValue(hashTable);
-            Console.WriteLine($"Key with highest value: {keyWithMaxValue}");
+            //    Dictionary<string, int> hashTable = new Dictionary<string, int>
+            //{
+            //    { "a", 1 },
+            //    { "b", 2 },
+            //    { "c", 3 },
+            //    { "d", 2 }
+            //};
+
+
+            //    string keyWithMaxValue = MaxValue(hashTable);
+            //    Console.WriteLine($"Key with highest value: {keyWithMaxValue}");
 
             #endregion
+
+            #region 4-
+
+            //string[] strs = { "eat", "tea", "tan", "ate", "nat", "bat" };
+
+            //var groupedAnagrams = Group(strs);
+
+            //foreach (var group in groupedAnagrams)
+            //{
+            //    Console.WriteLine(string.Join(", ", group));
+            //}
+
+            #endregion
+
         }
     }
 }
